32b2d1ce91b4b9917ada4235eb321b82d9f37fa7
eriksanchez <erikcsanchezdev@gmail.com>
2020-09-04 23:07:01 -0500

    Intro skelaton made...

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Intro.css";
+import { Icon } from "semantic-ui-react";
+
 function Intro() {
   return (
     <div class="Intro">
       <img src="/ES_logo.png" alt="logo" class="ES_logo" />
       <img src="/ErikSanchezTitle.png" alt="Erik Sanchez" class="Title" />
+      <img
+        src="/SoftwareEngineer.png"
+        alt="Software Engineer"
+        class="SE_title"
+      />
+      <div>
+        <Icon name="linkedin" size="huge" />
+        <Icon name="github" size="huge" />
+        <Icon name="twitter square" size="huge" />
+        <Icon name="mail" size="huge" />
+      </div>
     </div>
   );
 }
