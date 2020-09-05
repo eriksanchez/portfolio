11dc271b5821b50990082645fd6255b57e93656b
eriksanchez <erikcsanchezdev@gmail.com>
2020-09-04 22:44:15 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Intro from "./components/Intro";
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Intro />
+      <About />
+      <Resume />
+      <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Intro/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Intro.css";
+function Intro() {
+  return (
+    <div class="Intro">
+      <img src="/ES_logo.png" alt="logo" class="ES_logo" />
+      <img src="/ErikSanchezTitle.png" alt="Erik Sanchez" class="Title" />
+    </div>
+  );
+}
+export default Intro;
