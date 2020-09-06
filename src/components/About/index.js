8784f27aac38a380b26142ad209df096ebb669e6
eriksanchez <erikcsanchezdev@gmail.com>
2020-09-05 19:06:39 -0500

    About me...

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,23 @@
 import React from "react";
-
+import "./About.css";
 function About() {
   return (
     <div>
-      <h1>About Me</h1>
+      <h1>Who Am I?</h1>
+
+      <div style={{ backgroundColor: "red" }}>
+        <p>
+          {" "}
+          I am a results driven motivated Software Engineer with professional
+          experience creating full stack web and mobile applications.
+        </p>
+      </div>
+
+      <div>
+        <img src="/JavascriptImage.png" alt="javascript" class="image" />
+        <img src="/nodeImage.jpg" alt="node" class="image" />
+        <img src="/ReactImage.png" alt="react" class="image" />
+      </div>
     </div>
   );
 }
