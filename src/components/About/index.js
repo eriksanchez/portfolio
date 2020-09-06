18077a55dc43eea2a31ba0b3db7a7089543a4bdc
eriksanchez <erikcsanchezdev@gmail.com>
2020-09-05 22:20:17 -0500

    About me skeleton made...

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,67 +1,72 @@
 import React from "react";
 import "./About.css";
-import { Button } from "semantic-ui-react";
+import { Grid, Image, Button } from "semantic-ui-react";
 import SkillCard from "./SkillCard";
 
 function About() {
   return (
     <div>
       <h1>Who Am I?</h1>
-      <div style={{ display: "flex" }}>
-        <div class="Introduction">
-          <p>
-            I am a results driven motivated Software Engineer with professional
-            experience creating full stack web and mobile applications.
-          </p>
-        </div>
 
-        <div>
-          <img src="/JavascriptImage.png" alt="javascript" class="image" />
-          <img src="/nodeImage.jpg" alt="node" class="image" />
-          <img src="/ReactImage.png" alt="react" class="image" />
-        </div>
-      </div>
+      <Grid divided="vertically">
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            <p>
+              I am a results driven motivated Software Engineer with
+              professional experience creating full stack web and mobile
+              applications.
+            </p>
+          </Grid.Column>
+          <Grid.Column>
+            <img src="/JavascriptImage.png" alt="javascript" class="image" />
+            <img src="/nodeImage.jpg" alt="node" class="image" />
+            <img src="/ReactImage.png" alt="react" class="image" />
+          </Grid.Column>
+        </Grid.Row>
 
-      <div class="ui grid">
-        <div class="four wide column">
-          <SkillCard
-            imageSrc="/websiteImage.png"
-            FieldTitle="FrontEnd"
-            SkillOne="HTML5"
-            SkillTwo="CSS3 | SASS"
-            SkillThree="Javascript"
-            SkillFour="React | Redux"
-          />
-        </div>
-        <div class="four wide column">
-          <SkillCard
-            imageSrc="/ServerIcon.jpg"
-            FieldTitle="Server"
-            SkillOne="NodeJS"
-            SkillTwo="Express"
-            SkillThree="Python"
-          />
-        </div>
-        <div class="four wide column">
-          <SkillCard
-            imageSrc="/DatabaseIcon.png"
-            FieldTitle="Database"
-            SkillOne="PostgreSQL"
-            SkillTwo="MySQL"
-            SkillThree="MongoDB"
-          />
-        </div>
-        <div class="four wide column">
-          <SkillCard
-            imageSrc="/CloudIcon.png"
-            FieldTitle="Deployment"
-            SkillOne="Firebase"
-            SkillTwo="AWS"
-            SkillThree="Docker"
-          />
-        </div>
-      </div>
+        <Grid.Row columns={4}>
+          <Grid.Column>
+            <SkillCard
+              imageSrc="/websiteImage.png"
+              FieldTitle="FrontEnd"
+              SkillOne="HTML5"
+              SkillTwo="CSS3 | SASS"
+              SkillThree="Javascript"
+              SkillFour="React | Redux"
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <SkillCard
+              imageSrc="/ServerIcon.jpg"
+              FieldTitle="Server"
+              SkillOne="NodeJS"
+              SkillTwo="Express"
+              SkillThree="Python"
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <SkillCard
+              imageSrc="/DatabaseIcon.png"
+              FieldTitle="Database"
+              SkillOne="PostgreSQL"
+              SkillTwo="MySQL"
+              SkillThree="MongoDB"
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <SkillCard
+              imageSrc="/CloudIcon.png"
+              FieldTitle="Deployment"
+              SkillOne="Firebase"
+              SkillTwo="AWS"
+              SkillThree="Docker"
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+
       <Button primary>Download Resume</Button>
+      <h4>Or take a quick look below </h4>
     </div>
   );
 }
