64167ce34a27d53e07ae7df7227a587a754eaf55
eriksanchez <erikcsanchezdev@gmail.com>
2020-09-07 22:53:16 -0500

    Resume info updated...

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -30,16 +30,102 @@ function Resume() {
             <h1>Work Experience</h1>
             <Timeline lineColor="black">
               <Event
-                interval={{ start: 2009, end: 2013 }}
-                title="Deer Park High School"
-                subtitle="Deer Park, Tx, USA"
-              ></Event>
-              <Event
-                interval="2013 – 2017"
-                title="The University of Texas at Tyler"
+                interval={{ start: 2019, end: 2020 }}
+                title="Software Developer"
+                subtitle="Zerynth (Pisa, Italy)"
               >
-                B.S. Computer Science, <br />
-                Mathematics Minor
+                <ul>
+                  <li>
+                    Developed, and tested a web application for Toscana Riporti
+                    Srl.
+                  </li>
+                  <li>
+                    Responsible for front end development implementing reusable
+                    components in React and using Redux for state management.
+                  </li>
+                  <li>
+                    Also responsible for creating a REST API and writing up the
+                    documentation.
+                  </li>
+                </ul>
+              </Event>
+              <Event
+                interval="2017 – 2018"
+                title="Mobile Application Developer"
+                subtitle="Houston FC (Houston, Tx, USA)"
+              >
+                <ul>
+                  <li>Developed UI for Houston FC mobile application. </li>
+                  <li>
+                    Helped launch an application to give Houston FC fans an
+                    interactive team experience providing up-to date scores,
+                    schedule, news, and statistics.
+                  </li>
+                  <li>
+                    Responsible for front-end development of application.
+                    (JavaScript, React Native).
+                  </li>
+                </ul>
+              </Event>
+              <Event
+                interval="2017"
+                title="Electrician Apprentice"
+                subtitle="Bartec, Formosa Plastics Corporations (Point Comfort, Texas, USA)"
+              >
+                <ul>
+                  <li>
+                    Installed and connected power supply wiring, cables,
+                    conduit, and electrical apparatuses for machines and
+                    equipment in new and existing facilities following diagrams,
+                    schematics, or blueprints.
+                  </li>
+                  <li>
+                    Inspected and tested components of electrical systems,
+                    including transfer switches and switch gears.
+                  </li>
+                  <li>
+                    Ran wire, and electrical conduit along high altitude cable
+                    tray.
+                  </li>
+                </ul>
+              </Event>
+              <Event
+                interval="2015"
+                title="Electrician Apprentice"
+                subtitle="Meitec Inc. (Houston, Texas, USA) "
+              >
+                <ul>
+                  <li>
+                    Installed and repaired wiring, electrical fixtures, power
+                    equipment, and components of machinery.
+                  </li>
+                  <li>Surveyed electrical systems, circuits and plans.</li>
+                  <li>
+                    Measured, cut, bent, threaded, assembled, and installed
+                    electrical conduit using rulers, measuring devices, hand
+                    tools, pipe threader, and conduit bender.
+                  </li>
+                </ul>
+              </Event>
+              <Event
+                interval="2014-2017"
+                title="Audio Engineer"
+                subtitle="The Sound of E Productions (Tyler, Texas, USA)  "
+              >
+                <ul>
+                  <li>
+                    Responsible for mixing, mastering, editing and producing
+                    musical compositions.
+                  </li>
+                  <li>
+                    Worked with a variety of Digital Audio Workstations such as
+                    Pro Tools, Ableton Live, FL Studios, and Logic Pro.
+                  </li>
+                  <li>
+                    Operated recording equipment, and set sound mixing
+                    board-control levels.
+                  </li>
+                </ul>
               </Event>
             </Timeline>
           </Grid.Column>
