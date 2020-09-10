583d73ad8740c98dada0f0ad0651a37c25287e63
eriksanchez <erikcsanchezdev@gmail.com>
2020-09-10 13:39:26 -0500

    Starting back-end...

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,14 +1,59 @@
 import React, { useState } from "react";
 import "./Contact.css";
+import axios from "axios";
 
 function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [name, setName] = useState();
+  const [lastname, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+  const [sent, setSent] = useState();
 
-  function SubmitButton() {
-    console.log("This is a console log");
-    console.log(name, email, message);
+  function handleName(e) {
+    setName(e.target.value);
+  }
+
+  function handleLastname(e) {
+    setLastName(e.target.value);
+  }
+
+  function handleEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleMessage(e) {
+    setMessage(e.target.value);
+  }
+
+  function resetForm() {
+    setName("");
+    setLastName("");
+    setEmail("");
+    setMessage("");
+  }
+
+  setTimeout(() => {
+    setSent(false);
+  }, 3000);
+
+  function formSubmit(e) {
+    e.preventDefault();
+    let data = {
+      name: name,
+      lastname: lastname,
+      email: email,
+      message: message,
+    };
+
+    axios
+      .post("/api/form", data)
+      .then((res) => {
+        setSent(true);
+        resetForm();
+      })
+      .catch(() => {
+        console.log("Message not sent");
+      });
   }
   return (
     <div className="container">
@@ -22,6 +67,8 @@ function Contact() {
             name="name"
             className="name"
             placeholder="your name..."
+            value={name}
+            onChange={handleName}
           />
         </div>
         {/*end of single item*/}
@@ -34,6 +81,8 @@ function Contact() {
             name="lastname"
             className="lastname"
             placeholder="your lastname..."
+            value={lastname}
+            onChange={handleLastname}
           />
         </div>
         {/*end of single item*/}
@@ -45,6 +94,8 @@ function Contact() {
             name="email"
             className="email"
             placeholder="your email..."
+            value={email}
+            onChange={handleEmail}
           />
         </div>
         {/*end of single item*/}
@@ -57,6 +108,8 @@ function Contact() {
             cols="30"
             rows="5"
             placeholder="your message..."
+            value={message}
+            onChange={handleMessage}
           ></textarea>
         </div>
         {/*end of single item*/}
