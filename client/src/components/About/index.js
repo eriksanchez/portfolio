5b677ce4e96a4417beb569077afae59def5bd21f
eriksanchez <erikcsanchezdev@gmail.com>
2020-09-16 10:47:34 -0500

    GSAP animation testing...

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./About.css";
-import { Grid, Button } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
+import { TimelineLite, TweenMax, Power3, gsap } from "gsap";
 import SkillCard from "./SkillCard";
 
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerEase(ScrollTrigger);
+
 function About() {
+  let t1 = new TimelineLite({ delay: 0.3 });
+  useEffect(() => {
+    {
+      t1.from(
+        ".About_description",
+        { y: 15, opacity: 0, ease: Power3.easeOut, delay: 0.2 },
+        "Start"
+      );
+      t1.staggerFrom(
+        ".image",
+        1,
+        { y: 30, ease: Power3.easeOut, opacity: 0 },
+        0.15,
+        "Start"
+      )
+        .from(".About_skillcards", {
+          y: 20,
+          opacity: 0,
+          ease: Power3.easeOut,
+          delay: 0.2,
+        })
+        .from(
+          ".btn-resume",
+          {
+            y: 10,
+            opacity: 0,
+            ease: Power3.easeOut,
+            delay: 0.2,
+          },
+          0.35
+        );
+    }
+  }, []);
+
   return (
     <div class="About_container">
       <div class="About_heading">
@@ -57,7 +95,9 @@ function About() {
         />
       </div>
 
-      <Button primary>Download Resume</Button>
+      <Button class="btn-resume" primary>
+        Download Resume
+      </Button>
       <h4>Or take a quick look below </h4>
     </div>
   );
